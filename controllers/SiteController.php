49c08fdcef0460515f25cb95bef1edfe7b4b51dd
redesc <p.a.zakharov@gmail.com>
2020-11-20 22:09:31 +0800

    Add registration, add files assets

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -9,6 +9,7 @@ use yii\web\Response;
 use yii\filters\VerbFilter;
 use app\models\LoginForm;
 use app\models\ContactForm;
+use app\models\SignupForm;
 
 class SiteController extends Controller
 {
@@ -85,6 +86,31 @@ class SiteController extends Controller
             'model' => $model,
         ]);
     }
+
+    
+
+/**
+ * Форма регистрации.
+ *
+ * @return mixed
+ */
+public function actionSignup()
+{
+    $model = new SignupForm();
+    if ($model->load(Yii::$app->request->post())) {
+        if ($user = $model->signup()) {
+            if (Yii::$app->getUser()->login($user)) {
+                return $this->goHome();
+            }
+        }
+    }
+
+    return $this->render('signup', [
+        'model' => $model,
+    ]);
+}
+
+
 
     /**
      * Logout action.
