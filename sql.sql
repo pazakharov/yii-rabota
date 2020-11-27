7f3bbcdc0dc4f4c0ffaec8b9b291a11a9addafa8
redesc <p.a.zakharov@gmail.com>
2020-11-27 10:46:55 +0800

    Well Done before upload

---
+++ sql.sql
@@ -0,0 +1,148 @@
+-- --------------------------------------------------------
+-- Хост:                         127.0.0.1
+-- Версия сервера:               10.4.11-MariaDB - mariadb.org binary distribution
+-- Операционная система:         Win64
+-- HeidiSQL Версия:              11.1.0.6116
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+-- Дамп структуры для таблица yii2rabota.grafik
+CREATE TABLE IF NOT EXISTS `grafik` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL DEFAULT '',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
+
+-- Экспортируемые данные не выделены.
+
+-- Дамп структуры для таблица yii2rabota.migration
+CREATE TABLE IF NOT EXISTS `migration` (
+  `version` varchar(180) NOT NULL,
+  `apply_time` int(11) DEFAULT NULL,
+  PRIMARY KEY (`version`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- Экспортируемые данные не выделены.
+
+-- Дамп структуры для таблица yii2rabota.opyt
+CREATE TABLE IF NOT EXISTS `opyt` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `resume_id` int(11) NOT NULL,
+  `date1` date NOT NULL,
+  `date2` date NOT NULL,
+  `organization` varchar(255) NOT NULL DEFAULT '',
+  `position` varchar(255) NOT NULL DEFAULT '',
+  `duties` text DEFAULT NULL,
+  `month1` int(11) NOT NULL DEFAULT 0,
+  `year1` int(11) NOT NULL DEFAULT 0,
+  `month2` int(11) NOT NULL DEFAULT 0,
+  `year2` int(11) NOT NULL DEFAULT 0,
+  PRIMARY KEY (`id`),
+  KEY `resume_id` (`resume_id`),
+  CONSTRAINT `FK_opyt_resume` FOREIGN KEY (`resume_id`) REFERENCES `resume` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=71 DEFAULT CHARSET=utf8;
+
+-- Экспортируемые данные не выделены.
+
+-- Дамп структуры для таблица yii2rabota.resume
+CREATE TABLE IF NOT EXISTS `resume` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `author_id` int(11) NOT NULL,
+  `first_name` varchar(255) NOT NULL,
+  `middle_name` varchar(255) NOT NULL,
+  `last_name` varchar(255) NOT NULL,
+  `birthdate` date NOT NULL,
+  `sex` varchar(255) NOT NULL,
+  `city` varchar(255) NOT NULL,
+  `mail` varchar(255) NOT NULL,
+  `phone` varchar(255) NOT NULL,
+  `specialization_id` int(11) NOT NULL,
+  `about` text DEFAULT NULL,
+  `foto` varchar(255) NOT NULL,
+  `zp` int(11) NOT NULL DEFAULT 0,
+  `created_at` int(11) DEFAULT NULL,
+  `updated_at` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `author_id` (`author_id`),
+  KEY `specialization_id` (`specialization_id`),
+  CONSTRAINT `FK_resume_specializations` FOREIGN KEY (`specialization_id`) REFERENCES `specializations` (`id`) ON UPDATE CASCADE,
+  CONSTRAINT `FK_resume_user` FOREIGN KEY (`author_id`) REFERENCES `user` (`id`) ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8;
+
+-- Экспортируемые данные не выделены.
+
+-- Дамп структуры для таблица yii2rabota.resumegrafik
+CREATE TABLE IF NOT EXISTS `resumegrafik` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `resume_id` int(11) NOT NULL,
+  `grafik_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `resume_id` (`resume_id`),
+  KEY `grafik_id` (`grafik_id`),
+  CONSTRAINT `FK_resumegrafik_grafik` FOREIGN KEY (`grafik_id`) REFERENCES `grafik` (`id`),
+  CONSTRAINT `FK_resumegrafik_resume` FOREIGN KEY (`resume_id`) REFERENCES `resume` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=442 DEFAULT CHARSET=utf8;
+
+-- Экспортируемые данные не выделены.
+
+-- Дамп структуры для таблица yii2rabota.resumezanyatost
+CREATE TABLE IF NOT EXISTS `resumezanyatost` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `resume_id` int(11) NOT NULL,
+  `zanyatost_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `resume_id` (`resume_id`),
+  KEY `zanyatost_id` (`zanyatost_id`),
+  CONSTRAINT `FK_resumezanyatost_resume` FOREIGN KEY (`resume_id`) REFERENCES `resume` (`id`),
+  CONSTRAINT `FK_resumezanyatost_zanaytost` FOREIGN KEY (`zanyatost_id`) REFERENCES `zanaytost` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=228 DEFAULT CHARSET=utf8;
+
+-- Экспортируемые данные не выделены.
+
+-- Дамп структуры для таблица yii2rabota.specializations
+CREATE TABLE IF NOT EXISTS `specializations` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=27 DEFAULT CHARSET=utf8;
+
+-- Экспортируемые данные не выделены.
+
+-- Дамп структуры для таблица yii2rabota.user
+CREATE TABLE IF NOT EXISTS `user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `auth_key` varchar(32) COLLATE utf8_unicode_ci NOT NULL,
+  `password_hash` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `password_reset_token` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `email` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `created_at` int(11) NOT NULL,
+  `updated_at` int(11) NOT NULL,
+  `is_admin` smallint(6) DEFAULT 0,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `username` (`username`),
+  UNIQUE KEY `email` (`email`),
+  UNIQUE KEY `password_reset_token` (`password_reset_token`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- Экспортируемые данные не выделены.
+
+-- Дамп структуры для таблица yii2rabota.zanaytost
+CREATE TABLE IF NOT EXISTS `zanaytost` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
+
+-- Экспортируемые данные не выделены.
+
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
