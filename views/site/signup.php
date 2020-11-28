e21d33328cf5ea429790349627e84c81368b3427
redesc <p.a.zakharov@gmail.com>
2020-11-28 20:15:08 +0800

    renamed vars

--- views/site/signup.php
+++ views/site/signup.php
@@ -6,6 +6,9 @@
 
 use yii\helpers\Html;
 use yii\bootstrap\ActiveForm;
+use app\assets\SignUpAsset;
+
+SignUpAsset::register($this);
 
 $this->title = 'Регистрация';
 $this->params['breadcrumbs'][] = $this->title;
@@ -15,17 +18,23 @@ $this->params['breadcrumbs'][] = $this->title;
         <div class="site-signup">
             <h1 class="main-title mt24 mb16"><?= Html::encode($this->title) ?></h1>
 
-            <div class="paragraph-lead mb16"> <span>  Заполните поля для регистрации:</span> </div>
+            <div class="paragraph-lead mb16"> <span> Заполните поля для регистрации:</span> </div>
 
             <div class="row">
                 <div class="col-lg-5">
                     <?php $form = ActiveForm::begin(['id' => 'form-signup']); ?>
 
-                    <?= $form->field($model, 'username')->textInput(['autofocus' => true]) ?>
+                    <div class="input-wrap">
+                        <?= $form->field($model, 'username')->textInput(['autofocus' => true]) ?>
+                    </div>
 
-                    <?= $form->field($model, 'email') ?>
+                    <div class="input-wrap">
+                        <?= $form->field($model, 'email') ?>
+                    </div>
 
-                    <?= $form->field($model, 'password')->passwordInput() ?>
+                    <div class="input-wrap">
+                        <?= $form->field($model, 'password')->passwordInput() ?>
+                    </div>
 
                     <div class="form-group">
                         <?= Html::submitButton('Регистрация', ['class' => 'btn btn-primary', 'name' => 'signup-button']) ?>
